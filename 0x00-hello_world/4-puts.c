4d0d455cf8ec30f5723ba0cea169bfe13797c3e0
tasnim <tasnim.omar2016@gmail.com>
2023-09-07 14:52:46 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  * Description: A C program that prints with puts function 
  *
  * Returns: always 0 (success)
- */
+*/
 
 int main(void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-       return(0);
+{	puts("\"Programming is like building a multilingual puzzle");
+  	return (0);
 }  
