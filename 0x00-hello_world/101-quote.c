6e2aea648622eebea470594aa24050b665315f3b
tasnim <tasnim.omar2016@gmail.com>
2023-09-09 16:10:11 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,15 @@
 
 /**
  * main - Entry point
- * Description: prints a quote using write function.
- * Return: Always 0 (Success)
+ * Description: prints a quote using write function
+ * 	size_t write(int fd, const void *buf, size_t count):
+ * Return: Always 1 (error)
  */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1. quo. 59);
 	return (1);
 }
