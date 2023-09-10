d13b7a741cfa4c3662d6e24babd5d3fe8dd980cc
tasnim <tasnim.omar2016@gmail.com>
2023-09-09 17:17:07 -0700

    9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print the lowercase alphabet
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	ch = 'z';
+	while (ch >= 'a')
+{
+	putchar(ch);
+	ch--;
+}
+	putchar('\n');
+	return (0);
+}
