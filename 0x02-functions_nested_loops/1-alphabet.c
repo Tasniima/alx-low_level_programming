abb95282754ee7bf12132c36c2447c64b51aa230
tasnim <tasnim.omar2016@gmail.com>
2023-09-12 14:56:06 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * Print_alphabet on the _putchar function
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#define"main.h"
+/**
+ * _islower function to check if character is lowercas
+ *
+ * @c: check inputs of function
+ *
+ * Return: return 1 if 'c' is lowercase
+ *              otherwise always 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	return (1);
+		return (0);
+}
