cc2e5eeed162e72da433e180baa36281e5732b32
tasnim <tasnim.omar2016@gmail.com>
2023-09-12 15:28:34 -0700

    5

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * Print_alphabet - Make the alphabet
+ * Print _alphabet - Make the alphabet
  *
- * Return: 1 if lowercase and 0 for otherwise
+ * Return:void
 */
 
 void print_alphabet(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
-
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
