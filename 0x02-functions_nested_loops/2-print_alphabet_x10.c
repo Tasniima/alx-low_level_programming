5b3f5ae0947ee200649397c1cadf913f4d66f9d9
tasnim <tasnim.omar2016@gmail.com>
2023-09-12 15:55:28 -0700

    6

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
- * _is lower function to check if character is lowercas
- *
- * @c: check inputs of function
- *
- * Return: return 1 if 'c' is lowercase
- *		otherwise always 0 (success)
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
 */
 
-int _is lower(int c)
+void print_alphabet_x10(void)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-		return (0);
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
 }
