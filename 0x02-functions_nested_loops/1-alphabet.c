a3b3129e4cd8ba88c6e53621ac8b628d9c138d02
tasnim <tasnim.omar2016@gmail.com>
2023-09-12 17:19:10 -0700

    8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _islower function - To check if character is lowercas
- * @c: Check inputs of function
- * Return: 1 for lowercase character or 0 for anything else
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
  */
-
 
 void print_alphabet(void)
 {
-char ch;
+char  ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
