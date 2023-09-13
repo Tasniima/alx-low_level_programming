cc88c165282074e3c8916f80f04428000c8a910e
tasnim <tasnim.omar2016@gmail.com>
2023-09-12 17:13:17 -0700

    9

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _islower function - to check if character is lowercas
- * @c: check inputs of function
+ * _islower function - To check if character is lowercas
+ * @c: Check inputs of function
  * Return: 1 for lowercase character or 0 for anything else
-*/
+ */
 
 
 void print_alphabet(void)
