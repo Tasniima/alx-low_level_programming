6d77126becde230b5f58f6fb8249ab97f91c4d24
tasnim <tasnim.omar2016@gmail.com>
2023-09-18 11:09:00 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nithing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+
+		for (i = 0; i < 1 / 2; i++)
+		{
+			temp = s[i];
+			s[i] = s[l - 1 - i];
+			s[l - 1 - i] = temp;
+		}
+}
