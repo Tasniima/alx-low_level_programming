b78dca38e4212ccb8c33b23b7f87a3378d419e50
tasnim <tasnim.omar2016@gmail.com>
2023-09-21 15:22:13 -0700

    5

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,9 +22,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src[i] != '\0')
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
