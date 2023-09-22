cae11d2999ae63049b7fc40d0a7d37f9f5c3537b
tasnim <tasnim.omar2016@gmail.com>
2023-09-22 14:44:54 -0700

    5

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - function that concatenates two string
@@ -6,7 +7,7 @@
  *@src: pointer to source input
  *
  * Return: Pointer to the resulting string
-*/
+ */
 
 char *_strcat(char *dest, char *src)
 {
