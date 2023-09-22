82d068db6b5263de03e93d30b41a4ee12f314877
tasnim <tasnim.omar2016@gmail.com>
2023-09-22 16:17:20 -0700

    g

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
+ * _strcat - Concatenates two strings
  * @dest: input value
- * Description: @src input value
+ * @src: input value
  * Return: Void
  */
 char *_strcat(char *dest, char *src)
