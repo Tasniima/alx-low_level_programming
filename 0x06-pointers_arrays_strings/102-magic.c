c39b0415450691cb1de204ec5c176fb3ea126b46
tasnim <tasnim.omar2016@gmail.com>
2023-09-21 19:44:18 -0700

    f

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -15,11 +15,10 @@ int main(void)
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
- * - you are not allowed to code anything elsw than this item
-	of code
+ * - you are not allowed to code anything else than this item of code
  */
-*(p + 5) = 98;
-/*...so that this prints 98\n */
-printf ("a[2] = %d\n", a[2]);
-return (0);
+ *(p + 5) = 98;
+	/*...so that this prints 98\n */
+	printf ("a[2] = %d\n", a[2]);
+	return (0);
 }
