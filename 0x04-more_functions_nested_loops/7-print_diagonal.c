56fff75a6a43de9e4b1048e04595efcc7760a9fa
tasnim <tasnim.omar2016@gmail.com>
2023-09-23 16:16:31 -0700

    f

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,19 @@
  */
 void print_diagonal(int n)
 {
-int i, j;
+	int i, j;
 
-if (n <= 0)
-{
-_putchar('\n');
-return;
-}
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
 }}
