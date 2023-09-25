f147719dca2a60515165033a6abf7427c31b1d6a
tasnim <tasnim.omar2016@gmail.com>
2023-09-25 16:40:25 -0700

    d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - fills memory with a constant byte.
+ * @s: pointer to put a constant
+ * @c: constant
+ * Return: Pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0, s[i] >= '\0', i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
