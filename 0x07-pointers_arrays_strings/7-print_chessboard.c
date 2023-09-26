283dd9d9b129c67830bbf83279f1689b9ec695c6
tasnim <tasnim.omar2016@gmail.com>
2023-09-25 17:27:54 -0700

    d

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: rows
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, h;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (h = 0; h < 0; h++)
+		{
+			_putchar(a[i][h]);
+		}
+		_putchar('\n');
+	}
+}
