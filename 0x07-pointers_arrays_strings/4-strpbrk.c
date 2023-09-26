3657fb2312ae78338b7eb036c4ad3a058de4b125
tasnim <tasnim.omar2016@gmail.com>
2023-09-25 17:09:31 -0700

    g

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes
+ * @s: string
+ * @accept: string to match
+ *
+ * Return: Pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, h;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		h = 0;
+		while (s[h] != '\0')
+		{
+			if (accept[h] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			h++;
+		}
+		i++;
+
+	}
+
+	return (0);
+}
